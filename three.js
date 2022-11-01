0a140f419ef39162c09e20d4f15499294c1a71bd
khizriev <askhab.askhabb.khizriev@mail.ru>
2022-11-01 13:43:16 +0300

    good

---
+++ three.js
@@ -0,0 +1,49 @@
+            //  ЗАДАЧА 1
+
+const numbar = 902;
+if ( numbar > 10  ) {
+    console.log('конста не меньше 10')
+} else {
+    console.log('конста меньше 10')
+}
+
+
+            //ЗАДАЧА-2 
+
+
+let text = 'содержимое переменной';
+if ( text === 'lorem'  ) {
+    console.log("у вас ошибка в тексте");
+}
+ else {
+    console.log ('содержимое переменной' + text);
+ }
+
+
+            //ЗАДАЧА-3 
+            
+let comment = 'lorem ipsum';
+if ( comment === length){ 
+    console.log('текст слишком длинный. В нем символов');}
+    else { 
+        console.log(comment.length);
+    }
+ 
+
+            //ЗАДАЧА-4
+
+ let cartSum = 6500;
+ let sum = 5000;
+
+ result = (6500 * 0.05);                    
+ if ( cartSum >= sum){
+
+ console.log(`ваша скидка ${result} Доставка бесплатно `);
+  } 
+  
+    else (cartSum < sum);
+    result = (5000 - 0.05) 
+  {
+ console.log(`Итог к оплате ${result} до скидки осталось ${sum}`);
+                          
+                        }
